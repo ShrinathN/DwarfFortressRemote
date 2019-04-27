70c9c60900eac669b57fdc8cb42cacb6769e4d85
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-04-28 01:12:54 +0530

    Base is done, TODO: add stuff

--- app/src/main/java/com/shinu/dwarffortressremote/MainActivity.java
+++ app/src/main/java/com/shinu/dwarffortressremote/MainActivity.java
@@ -1,21 +1,24 @@
 package com.shinu.dwarffortressremote;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    String string_global_address;
+    //default address
+    String global_string_address = "192.168.1.12";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MenuInflater menuInflater = getMenuInflater();
 
         //getting all the buttons
         Button button_upKey = findViewById(R.id.button_upKey);
@@ -41,8 +44,122 @@ public class MainActivity extends AppCompatActivity {
         button_upKey.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatagramSender datagramSender = new DatagramSender("#UP",
+                DatagramSender datagramSender = new DatagramSender("#UP", global_string_address);
+                datagramSender.sendData();
             }
         });
+
+        button_downKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("#DOWN", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_leftKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("#LEFT", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_rightKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("#RIGHT", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_plusKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("+", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_minusKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("-", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_layerUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender(">", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_layerDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("<", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("#RETURN", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_look.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("k", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("z", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+
+        button_designations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatagramSender datagramSender = new DatagramSender("d", global_string_address);
+                datagramSender.sendData();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_ipselector, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_item_selectip) {
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.layout_ipselector);
+            final EditText editText_ipselector_ip = dialog.findViewById(R.id.editText_ipselector_ip);
+            Button button_ipselector_setip = dialog.findViewById(R.id.button_ipselector_setip);
+            button_ipselector_setip.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    global_string_address = editText_ipselector_ip.getText().toString();
+                }
+            });
+            dialog.show();
+        }
+        return true;
     }
 }
