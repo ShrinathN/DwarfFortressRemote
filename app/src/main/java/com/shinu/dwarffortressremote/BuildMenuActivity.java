7753119ff59ef82fcda1857c3e18f69bccd87be4
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-05-05 02:01:40 +0530

    Fixed main activity, added landscape layout

--- app/src/main/java/com/shinu/dwarffortressremote/BuildMenuActivity.java
+++ app/src/main/java/com/shinu/dwarffortressremote/BuildMenuActivity.java
@@ -1,13 +1,27 @@
 package com.shinu.dwarffortressremote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BuildMenuActivity extends AppCompatActivity {
 
+    //macros
+    final int REQUEST_CODE_BUILD_RESULT = 1;
+
+    //global variables
+    static String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_menu);
+        
+    }
+
+    public void resultMethod() {
+        Intent intent = new Intent();
+        intent.putExtra("message", msg);
+        setResult(REQUEST_CODE_BUILD_RESULT, intent);
+        finish();
     }
 }
